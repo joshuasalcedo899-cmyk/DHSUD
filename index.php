0eaf96d39cdddf64eb5e385b5781525f21b75f25
Kim-Hori <nebreja.kmn14@gmail.com>
2026-02-02 16:24:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<body><center>
     <h1>DHSUD - MAIL TRACKER</h1>
     <h2>ADMIN</h2>
     <BUtton>ADMIN</BUtton>
