c35d28bdd252b1a6e5d59061297d61a3822d1971
_|osh.dev <joshuasalcedo899@gmail.com>
2026-02-20 14:38:04 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   main.css #	modified:   pages/Home_Page.php #

--- pages/Home_Page.php
+++ pages/Home_Page.php
@@ -209,12 +209,14 @@ for ($ri = 0; $ri < $rowCount; $ri++) {
 
 <body class="admin-home-bg">
     <div class="admin-home-header">
-        <div class="welcome-block">
-            <div style="font-size:1.2em;font-weight:600;color:#22336A;margin-top:5px;margin-bottom:2px;">Welcome, Admin!</div>
-            <a href="logout.php" class="logout-btn">Logout</a>
-        </div>
+        <div class="admin-home-header-main">
+            <div class="welcome-block">
+                <div style="font-size:1.2em;font-weight:600;color:#22336A;margin-top:5px;margin-bottom:2px;">Welcome, Admin!</div>
+                <a href="logout.php" class="logout-btn">Logout</a>
+            </div>
             <img src="../assets/Admin_HomePage_New.svg" alt="Admin Home Header" class="admin-home-header-img">
-            <div class="admin-home-header-border"></div>
+        </div>
+        <div class="admin-home-header-border"></div>
     </div>
         <!-- Edit Modal (hidden by default) -->
         <div id="editModalOverlay" class="edit-modal-overlay" style="display:none;">
